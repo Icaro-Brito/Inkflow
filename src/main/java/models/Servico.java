82bdd6c1b46fe21e03f0a1ce69538fcd3628dca9
CodeByWesley <desenvolvedorwesleymendonca@gmail.com>
2026-03-31 22:42:46 -0300

    adcionando models e classes

---
+++ src/main/java/models/Servico.java
@@ -0,0 +1,12 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // Gera Getter, Setter, equals, hashCode e toString
+@NoArgsConstructor // Gera construtor vazio
+@AllArgsConstructor // Gera construtor com todos os campos
+
+public class Servico {
+}
